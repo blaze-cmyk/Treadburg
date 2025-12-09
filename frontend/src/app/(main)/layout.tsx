494a8177dd2796c7422be308515be36ac067c7d4
Hariom Dhage <130223726+HariomDhage@users.noreply.github.com>
2025-12-10 02:57:57 +0530

    Remove navigation header, restore original sidebar-only layout

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Header from "@/components/header/Header";
 import Sidepanel from "@/components/sidepanel/Sidepanel";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import PageTransitionProvider from "@/components/providers/page-transition-provider";
@@ -18,9 +17,6 @@ export default function MainLayout({
         <Sidepanel />
       </div>
       <div className="flex-1 flex flex-col h-[100vh] relative overflow-hidden bg-background">
-        <div className="flex-shrink-0">
-          <Header />
-        </div>
         <div className="flex-1 flex flex-col min-h-0 overflow-hidden">
           <PageTransitionProvider>{children}</PageTransitionProvider>
         </div>
